8ab830f4bc9723e96393877b29e9a256a51eee4a
Dela Ricch <DelaRicch@gmail.com>
2022-08-20 14:00:43 +0000

    Done configuring Tailwind Css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import React from "react";
+
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#bcf75c] to-[#45f345]`
+}
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <section className={style.bg}>
+      <div className={style.container}>
+        <h2 className={style.heading}>Todo App</h2>
+      </div>
+    </section>
   );
 }
 
